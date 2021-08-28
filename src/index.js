a2df65dde70be27fad33d9010f82b0cc213b553e
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-08-28 05:40:01 -0500

    setup firebase context and lib files, seeded database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-render(<App />, document.getElementById('root'));
+
+render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>, 
+  document.getElementById('root')
+);
---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// IMPORT SEED FILE
+// import { seedDatabase } from '../seed';
+
+
+const config = {
+  apiKey: "AIzaSyBz6Ku8Rtv7gewepgDGjUZ0TbdbG607Xls",
+  authDomain: "instagram-b1fb0.firebaseapp.com",
+  projectId: "instagram-b1fb0",
+  storageBucket: "instagram-b1fb0.appspot.com",
+  messagingSenderId: "785144268210",
+  appId: "1:785144268210:web:ecb0d74232a078663150ab"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// CALL SEED FILE ***ONCE ONLY***
+// seedDatabase(firebase);
+
+console.log("firebase", firebase)
+
+export { firebase, FieldValue };
