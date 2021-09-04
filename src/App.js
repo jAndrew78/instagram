7cb8f60d73b5e40abe328eda1e91a1d5125d1405
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-09-04 15:40:55 -0500

    basic login page, initial setup of App.js with login route, minor edits (spacing/comments) elsewhere

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Login } from './pages';
+
+// lazy allows code splitting so client side doesn't have to take a big hit on what all it has to download
+const Login = lazy(() => import ('./pages/login'));
+
 export default function App() {
   return (
-    <h1>Instagram</h1>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
