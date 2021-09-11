fcd7c1e7a7ec02534d28cd17f5286f9deeace322
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-09-11 12:24:42 -0500

    login page setup - const variables useState, useContext, etc; imported pic

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 
 export default function Login() {
-  return <p>Login</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/assets/images/iphone-with-profile.png" alt="Iphone with Instagram App" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>Form</p>
+      </div>
+    </div>
+  );
 };
