f5c09585e46efc9506b6b7ed333ab3bba882aa1d
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-09-11 07:27:58 -0500

    removed pages/index -- doesn't make sense with lazy, created routes.js in constant, imported same to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { Login } from './pages';
+import * as ROUTES from './constants/routes';
 
 // lazy allows code splitting so client side doesn't have to take a big hit on what all it has to download
+// .js file won't come through as a bundle of all pages available. Just imports the page that's requested
 const Login = lazy(() => import ('./pages/login'));
 
 export default function App() {
@@ -10,7 +11,7 @@ export default function App() {
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path="/login" component={Login} />
+          <Route path={ROUTES.LOGIN} component={Login} />
         </Switch>
       </Suspense>
     </Router>
