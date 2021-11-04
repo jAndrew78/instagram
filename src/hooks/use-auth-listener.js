0694de06d475c7267ab52606f11f486cf2f1a35f
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-11-04 16:48:45 -0500

    created auth listener

---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, useContext } from 'react';
+import FirebaseContext from '../context/firebase';
+
+export default function useAuthListener() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const { firebase } = useContext(FirebaseContext);
+  
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChange((authUser) => {
+      if(authUser) {
+        // if user -> store user in local storage
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        // if !user -> clear local storage
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+
+    // cleanup
+    return () => listener();
+  }, [firebase])
+
+  return { user };
+}
