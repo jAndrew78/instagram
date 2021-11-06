0e2e2082acffa3b62ce00d42b7e1859e9c62cb69
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-11-06 12:14:08 -0500

    created getUserByUserId() function in services/firebase.js, created useUser hook for use in sidebar

---
+++ src/hooks/use-user.js
@@ -0,0 +1,22 @@
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/user';
+import { getUserByUserId } from '../services/firebase';
+
+
+export default function useUser() {
+  const [activeUser, setActiveUser] = useState({});
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getUserObjectByUserId() {
+      const res = await getUserByUserId(user.uid);
+      setActiveUser(res)
+    }
+
+    if (user?.uid) {
+      getUserObjectByUserId();
+    }
+  }, [user])
+
+  return { user: activeUser };
+}
